9b838589365f61adadb73871c0dafc32ad964747
maxfrancis1 <max.francis2000@gmail.com>
2018-11-27 15:05:56 +0000

    added tests and functions

--- src/fizzBuzz.java
+++ src/fizzBuzz.java
@@ -4,4 +4,20 @@ public class fizzBuzz {
         return "working";
     }
 
+    public String run(int[] num) {
+        String result = "";
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 15 == 0) {
+                result+= "FizzBuzz ";
+            } else if (num[i] % 5 == 0) {
+                result+= "Fizz ";
+            } else if (num[i] % 3 == 0) {
+                result+= "Buzz ";
+            } else {
+                result+= num[i] + " ";
+            }
+        }
+        return result.trim();
+    }
 }
+
--- tests/fizzBuzzTest.java
+++ tests/fizzBuzzTest.java
@@ -6,8 +6,24 @@ public class fizzBuzzTest {
 
     @Test
     public void initiate() {
-    fizzBuzz one = new fizzBuzz();
-    assertEquals("working", one.test());
+        fizzBuzz one = new fizzBuzz();
+        assertEquals("working", one.test());
+    }
+
+    @Test
+    public void testItPrintsNumbers() {
+        fizzBuzz two = new fizzBuzz();
+        int[] input = {2};
+        String result = two.run(input);
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void testItPrintsMultipleNumbersAndFizzBuzz() {
+        fizzBuzz two = new fizzBuzz();
+        int[] input = {2, 5, 3, 15};
+        String result = two.run(input);
+        assertEquals("2 Fizz Buzz FizzBuzz", result);
     }
 
 }
