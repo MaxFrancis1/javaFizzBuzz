b200d41b78c592b0a4ff23c5d2365b96b6c677ee
maxfrancis1 <max.francis2000@gmail.com>
2018-11-27 14:18:01 +0000

    small tests

--- src/fizzBuzz.java
+++ src/fizzBuzz.java
@@ -1,2 +1,7 @@
 public class fizzBuzz {
+
+    public String test() {
+        return "working";
+    }
+
 }
--- tests/fizzBuzzTest.java
+++ tests/fizzBuzzTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class fizzBuzzTest {
+
+    @Test
+    public void initiate() {
+    fizzBuzz one = new fizzBuzz();
+    assertEquals("working", one.test());
+    }
 
 }
